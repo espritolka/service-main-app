788cad10f5032308234f1b171922f05df8f0bcbf
Iugai Olga <espritolka@gmail.com>
2020-05-18 15:47:41 +0300

    + memo

--- src/components/logo/logo.jsx
+++ src/components/logo/logo.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import './logo.scss';
 
-const Logo = (props) => {
-
+const Logo = React.memo(function Logo(props){
     return (
         <div className='logo'>
              <p className='logo-text'> NAIL STUDIO</p>
             <div className='line'></div>
         </div>
     )
-}
+})
 
 export default Logo
